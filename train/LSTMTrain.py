f2b8da948488372593a8d21d4c182409ea6813d8
KiranM27 <kiran_bodhapati@mymail.sutd.edu.sg>
2022-04-20 18:31:55 +0800

    more refactoring

--- train/LSTMTrain.py
+++ train/LSTMTrain.py
@@ -1,4 +1,10 @@
-from dataclasses import dataclass
+from TrainHelpers import lstm_training_phase, lstm_testing_phase
+from models.LSTMModel import LSTM
+from Preprocessor import DataPreprocessor
+from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+import torch.nn as nn
+import Constants
 import sys
 from os.path import dirname, abspath
 
@@ -6,16 +12,6 @@ from train.TrainHelpers import lstm_testing_phase
 parent_dir_path = dirname(dirname(abspath(__file__)))
 sys.path.append(parent_dir_path)
 
-import Constants
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-import numpy as np
-from Preprocessor import DataPreprocessor
-from models.LSTMModel import LSTM
-from TrainHelpers import lstm_training_phase, lstm_testing_phase
 
 class TessDataset(Dataset):
     def __init__(self, x, y):
@@ -29,23 +25,24 @@ class TessDataset(Dataset):
     def __getitem__(self, index):
         return self.x[index], self.y[index]
 
+
 def get_accuracy(out, actual_labels):
     predictions = out.max(dim=1)[1]
     correct = (predictions == actual_labels).sum().item()
     accuracy = correct/Constants.LSTM_BATCH_SIZE
     return accuracy
 
-def train_lstm():
-  preprocessor = DataPreprocessor()
-  x_train, _, x_test, y_train, _, y_test = preprocessor.mfcc_data_prep("../data")
-  train_loader = DataLoader(TessDataset(x_train, y_train), batch_size=Constants.LSTM_BATCH_SIZE, shuffle=True)
-  test_loader = DataLoader(TessDataset(x_test, y_test), batch_size=Constants.LSTM_BATCH_SIZE, shuffle=True)
-  lstm_model = LSTM(Constants.LSTM_INPUT_SIZE, Constants.LSTM_HIDDEN_SIZE, Constants.LSTM_LAYER_SIZE, Constants.LSTM_OUTPUT_SIZE)
-  print('lstm_model: ', lstm_model)
-  criterion = nn.CrossEntropyLoss()
-  optimizer = optim.Adam(lstm_model.parameters(), lr=Constants.LSTM_LEARNING_RATE)
-  lstm_training_phase(lstm_model, train_loader, optimizer, criterion)
-  lstm_testing_phase(lstm_model, test_loader)
+def train_lstm(model_output_path):
+    preprocessor = DataPreprocessor()
+    x_train, _, x_test, y_train, _, y_test = preprocessor.mfcc_data_prep("../data")
+    train_loader = DataLoader(TessDataset(x_train, y_train), batch_size=Constants.LSTM_BATCH_SIZE, shuffle=True)
+    test_loader = DataLoader(TessDataset(x_test, y_test), batch_size=Constants.LSTM_BATCH_SIZE, shuffle=True)
+    lstm_model = LSTM(Constants.LSTM_INPUT_SIZE, Constants.LSTM_HIDDEN_SIZE, Constants.LSTM_LAYER_SIZE, Constants.LSTM_OUTPUT_SIZE)
+    print('lstm_model: ', lstm_model)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(lstm_model.parameters(), lr=Constants.LSTM_LEARNING_RATE)
+    lstm_training_phase(lstm_model, train_loader, optimizer, criterion)
+    lstm_testing_phase(lstm_model, test_loader, model_output_path)
 
 if __name__ == '__main__':
-    train_lstm()
+    train_lstm("LSTMModel.pt")
