b99294a76445cb714b2de564059ab38830f77fda
Anirudh181001 <shrivijiani@gmail.com>
2022-04-25 01:11:48 +0800

    Plots for CNN

--- Plot.py
+++ Plot.py
@@ -1,11 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+def format_acc_data(accs):
+    acc_format = []
+    for line in accs:
+        if len(line) <10:
+            continue
+        acc = line.split('(')[1].split(',')[0]
+        acc_format.append(float(acc))
+    return acc_format
 
 def generate_plot(train, val, filename, typ='loss', model='MLP', savepath='figs/'):
+    
     if len(train) != len(val):
         raise Exception("Input data arrays of different length.")
     
+    if typ == 'acc':
+        with open(train,'r') as f:
+            train = f.readlines()
+        with open(val,'r') as g:
+            val = g.readlines()
+        train = format_acc_data(train)
+        val = format_acc_data(val)
+    else:
+        train = np.genfromtxt(train, delimiter=',')
+        val = np.genfromtxt(val, delimiter=',')
     # params
     nepochs = len(train)
     title_fontsize = '18'
@@ -27,7 +46,8 @@ def generate_plot(train, val, filename, typ='loss', model='MLP', savepath='figs/
         ylabel = 'Accuracy'
         
     title = title + ' ' + '(' + model + ')'
-        
+    print("val max is ", max(val))
+    print("train max is ", max(train))
     xlabel = 'Epochs'
     plt.title(title, fontsize=title_fontsize)
     plt.xlabel(xlabel, fontsize=label_fontsize)
@@ -37,11 +57,9 @@ def generate_plot(train, val, filename, typ='loss', model='MLP', savepath='figs/
     plt.legend(fontsize=ytick_fontsize, bbox_to_anchor=(1.04,1), loc="upper left")
     plt.tight_layout()
     plt.savefig(savepath + filename + '.png', dpi=250)
-    # plt.show()
+    plt.show()
 
 if __name__ == "__main__":
-    train = np.genfromtxt('csv/dummy01.csv', delimiter=',')
-    val = np.genfromtxt('csv/dummy02.csv', delimiter=',')
 
     filename = 'LSTM_02'
-    generate_plot(train, val, filename, typ='loss', model='LSTM')
+    # generate_plot(train, val, filename, typ='loss', model='LSTM')
